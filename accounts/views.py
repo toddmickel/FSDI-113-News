0dbd5709a9f3c610150860720dd1ea1f43766104
Todd Mickel <todd@toddmickel.com>
2021-07-20 13:54:16 -0600

    End of Class 2

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.views import generic
+from django.urls import reverse_lazy
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    model = CustomUser
+    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
