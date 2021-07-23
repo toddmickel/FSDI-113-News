25ecc829472e9403a3da0a6a16efae138107566c
Todd Mickel <todd@toddmickel.com>
2021-07-22 21:09:08 -0600

    Heroku fix

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 DEFAULT_FROM_EMAIL = 'reset@toddmickel.com'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = NULL
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
